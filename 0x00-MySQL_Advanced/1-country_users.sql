1666ef551709b4d2fbc422f5e4f9a6ec16bd6d2b
isaaccleopas <isaaccleopas@gmail.com>
2023-08-09 14:07:00 +0000

    tasks 0 and 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+ -- Create a users table if it doesn't exist
+-- id, email, name and country
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
