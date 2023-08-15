0a8f7e1a2eba7748820905310a9fe361599ae8de
isaaccleopas <isaaccleopas@gmail.com>
2023-08-15 02:31:53 -0700

    task 11 added

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -3,6 +3,6 @@
 
 def schools_by_topic(mongo_collection, topic):
     """Returns topic searched"""
-    searched_topic = list(mongo_collection.find({"topic": topic}))
+    searched_topic = list(mongo_collection.find({"topics": topic}))
 
     return searched_topic
